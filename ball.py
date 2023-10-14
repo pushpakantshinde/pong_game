e82f77076f811aadc3ed8eeae0899b974058e677
Pushpakant Shindeclc <pushpakantshinde@gmail.com>
2023-10-14 22:15:13 +0530

    main updated

---
+++ ball.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color("blue")
+        self.speed("fastest")
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# start
+from turtle import Screen, Turtle
+from ball import Ball
+import time
+
+
+my_screen = Screen()
+my_screen.setup(width=800, height=600)
+my_screen.bgcolor("black")
+my_screen.title("My Ping-Pong Game")
+my_screen.tracer(0)
+
+
+game_ball = Ball()
+
+
+is_game_on = True
+
+while is_game_on:
+    pass
+
+
+
+
+
+my_screen.exitonclick()
